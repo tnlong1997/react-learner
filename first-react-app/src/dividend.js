5d845bc8f82fee6a9d896a1a9bfddbfb494252ea
tnlong1997 <tnlong1997@gmail.com>
2018-07-03 17:51:16 +0700

    Step 3

---
+++ first-react-app/src/dividend.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dividend extends Component {
+  render() {
+    return (
+      <div>
+        Dividend Component
+      </div>
+    );
+  }
+}
+
+export default Dividend;
--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Dividend from './dividend';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Dividend />, document.getElementById('root'));
 registerServiceWorker();
