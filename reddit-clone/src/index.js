ebd04d0f94324f325f8f14a99d874ea118175c16
tnlong1997 <tnlong1997@gmail.com>
2018-07-06 17:25:14 +0700

    Init reddit clone project

---
+++ reddit-clone/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/app';
+import registerServiceWorker from './utils/registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
