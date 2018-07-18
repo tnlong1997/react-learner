efd10c0059cba04da9cdc77cdc6a424a509909f8
tnlong1997 <tnlong1997@gmail.com>
2018-07-18 17:28:40 +0700

    Done

--- child-parent-component-practice/src/App.js
+++ child-parent-component-practice/src/App.js
@@ -3,19 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+	constructor(props) {
+		super(props);
+		this.clicked = this.clicked.bind(this);
+		this.state = {
+			hit: true,
+		}
+	}
+
+	clicked = () => {
+		let t = true;
+		if (this.state.hit) {
+			t = false;
+		}
+		this.setState({
+			hit: t,
+		});
+	}
+
+  	render() {
+    	return (
+			<div>
+    			<Child1 clicked={this.clicked}></Child1>
+				{this.state.hit && <Child2/>}
+			</div>
+    	);
   }
+}
+
+class Child1 extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<button onClick={this.props.clicked}> Clicked here </button>
+		)
+	}
+}
+
+class Child2 extends Component {
+	render() {
+		return (
+			<div style={{width: 100, height: 100, backgroundColor: "red"}}></div>
+		)
+	}
 }
 
 export default App;
