9da12f027627a101b2a2feaf0c6f07bb5aa9baf8
Long To <lto@draftkings.com>
2019-12-27 13:38:17 -0500

    Finished tutorials

--- react-redux-tutorial/src/redux/store/index.js
+++ react-redux-tutorial/src/redux/store/index.js
@@ -6,16 +6,23 @@
  * @Last modified time: 2019-12-21T18:36:49-05:00
  */
 
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducers/index";
-import {forbiddenWordsMiddleware} from "../middlewares";
-import thunk from "redux-thunk";
+ import { createStore, applyMiddleware, compose } from "redux";
+ import rootReducer from "../reducers/index";
+ import { forbiddenWordsMiddleware } from "../middleware";
+ import createSagaMiddleware from "redux-saga";
+ import apiSaga from "../sagas/api-saga";
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const initialiseSagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-	rootReducer,
-	storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
-);
+ const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default store;
+ const store = createStore(
+   rootReducer,
+   storeEnhancers(
+     applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddlewarse)
+   )
+ );
+
+ initialiseSagaMiddleware.run(apiSaga);
+
+ export default store;
